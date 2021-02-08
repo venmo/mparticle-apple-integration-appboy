3fe120008912ed1f20617746b5cf6d39ccb8d800
Mark Adams <markadams@paypal.com>
2021-02-08 12:56:05 -0800

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mParticle-Appboy",
+    platforms: [ .iOS(.v9), .tvOS(.v9) ],
+    products: [
+        .library(
+            name: "mParticle-Appboy",
+            targets: ["mParticle-Appboy"]),
+    ],
+    dependencies: [
+      .package(name: "mParticle-Apple-SDK",
+               url: "https://github.com/mParticle/mparticle-apple-sdk",
+               .upToNextMajor(from: "8.0.0")),
+      .package(name: "Adjust",
+               url: "https://github.com/Appboy/appboy-ios-sdk",
+               .upToNextMajor(from: "3.27.0")),
+    ],
+    targets: [
+        .target(
+            name: "mParticle-Appboy",
+            dependencies: [
+                "mParticle-Apple-SDK",
+                "Appboy_iOS_SDK"
+            ],
+            path: "mParticle-Appboy",
+            publicHeadersPath: "."
+        )
+    ],
+    cxxLanguageStandard: .cxx11
+)
